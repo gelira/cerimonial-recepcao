1194a5820b99c5e5e22d6fe4a0b7d9f1d08f113f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2024-08-31 11:38:35 -0300

    chore: sort list

--- backend/src/main/java/com/cerimonial/backend/repositories/GuestRepository.java
+++ backend/src/main/java/com/cerimonial/backend/repositories/GuestRepository.java
@@ -3,10 +3,14 @@ package com.cerimonial.backend.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.cerimonial.backend.models.Guest;
 
 public interface GuestRepository extends MongoRepository<Guest, String> {
+    @Query(sort = "{name: 1}")
     List<Guest> findByEventId(String eventId);
+
+    @Query(sort = "{name: 1}")
     List<Guest> findByTableId(String tableId);
 }
--- backend/src/main/java/com/cerimonial/backend/repositories/TableRepository.java
+++ backend/src/main/java/com/cerimonial/backend/repositories/TableRepository.java
@@ -3,9 +3,11 @@ package com.cerimonial.backend.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.cerimonial.backend.models.Table;
 
 public interface TableRepository extends MongoRepository<Table, String> {
+    @Query(sort = "{identifier: 1}")
     List<Table> findByEventId(String eventId);
 }
