551a5c7824cc1fad59df091ed2f03a2137ac1d4b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2024-09-06 21:06:10 -0300

    chore: fix identation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,33 +5,37 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    vue(),
+    vueJsx(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'my-vue-app',
-      short_name: 'my-vue-app',
-      description: 'my-vue-app',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'my-vue-app',
+        short_name: 'my-vue-app',
+        description: 'my-vue-app',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ],
 })
