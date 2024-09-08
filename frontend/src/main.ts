237f35202fe7c2f9598ed5e3b0174ddad03bb429
Geraldo Eloi <geraldo.eloi11@gmail.com>
2024-09-08 15:51:42 -0300

    chore: install vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,16 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
+import 'vuetify/styles'
+
 import App from './App.vue'
-import router from './router'
+import router from './plugins/router'
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
