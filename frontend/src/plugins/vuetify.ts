fae898efc7b9c1884ace81754bf458a0e5efcd3d
Geraldo Eloi <geraldo.eloi11@gmail.com>
2024-09-29 10:15:45 -0300

    chore: iconset material design

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,13 +1,21 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
 
-import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
 })
 
 export default vuetify
