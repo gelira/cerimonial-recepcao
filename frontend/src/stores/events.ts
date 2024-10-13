6fd5e0632898bd727a3d0d908ed83ed3159f77d9
Geraldo Eloi <geraldo.eloi11@gmail.com>
2024-10-12 22:45:52 -0300

    chore: sort events

--- frontend/src/stores/events.ts
+++ frontend/src/stores/events.ts
@@ -17,11 +17,32 @@ export const useEventsStore = defineStore('events', () => {
   const state = reactive<State>({ events: [] })
   
   const events = computed(() => state.events)
-  
-  function fetchEvents() {
-    apiFetchEvents()
-      .then((data) => state.events = data.events)
-      .catch(() => state.events = [])
+
+  function getEventLocalStorage(eventId: string) {
+    const strData = localStorage.getItem(`event-${eventId}`)
+
+    if (!strData) {
+      return null
+    }
+
+    return JSON.parse(strData) as IEventLocalStorage
+  }
+
+  function sortByLocalStorage(events?: IEvent[]) {
+    state.events = (events ?? state.events).sort((a, b) => {
+      const aExists = !!getEventLocalStorage(a.id)
+      const bExists = !!getEventLocalStorage(b.id)
+
+      if (aExists && !bExists) {
+        return -1
+      }
+
+      if (!aExists && bExists) {
+        return 1
+      }
+
+      return a.name.localeCompare(b.name)
+    })
   }
 
   function updateEvent(event: IEvent) {
@@ -59,20 +80,20 @@ export const useEventsStore = defineStore('events', () => {
     }
 
     localStorage.setItem(`event-${event.id}`, JSON.stringify(eventToSave))
+
+    sortByLocalStorage()
   }
 
   function deleteEventLocalStorage(event: IEvent) {
     localStorage.removeItem(`event-${event.id}`)
+
+    sortByLocalStorage()
   }
 
-  function getEventLocalStorage(eventId: string) {
-    const strData = localStorage.getItem(`event-${eventId}`)
-
-    if (!strData) {
-      return null
-    }
-
-    return JSON.parse(strData) as IEventLocalStorage
+  function fetchEvents() {
+    apiFetchEvents()
+      .then((data) => sortByLocalStorage(data.events))
+      .catch(() => state.events = [])
   }
 
   return {
